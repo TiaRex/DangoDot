50fa06dc3c00a342bbc69a1e501193eef76babe5
Tia Truong <tiatruongti@gmail.com>
2024-07-09 13:30:48 -0500

    Formatted Favorites Box

--- src/pages/Watchlist.js
+++ src/pages/Watchlist.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
+import '../styles/Watchlist.css';
 
 const Watchlist = () => {
   return (
-    <div>
+    <body>
         <Navbar />
-        <h1>Watchlist</h1>
-    </div>
+        <header>
+            <div class="header-container">
+                <h1 class="header-title">My Watchlists</h1>
+            </div>
+        </header>
+
+        <main>
+          <section class="watchlist-container" id="watchlist-container">
+            {/* Favorites */}
+            <h1 class="favorites-heading">Favorites</h1>
+            <div class="favorites-box">
+              {/* <img src=images/demonslayer.jpg></img> */}
+            </div>
+          </section>
+        </main>
+    </body>
   )
 }
 
