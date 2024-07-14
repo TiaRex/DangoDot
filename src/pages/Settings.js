6b0b6273517ece74a097c34ad9e37fa91d9bc73b
locationcyberia <brianna.corker@protonmail.com>
2024-07-14 00:55:21 -0400

    update page layout

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,13 +1,61 @@
-import React from 'react'
-import Navbar from '../components/Navbar';
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "../styles/settings.css";
+
+import Dangologo from "../assets/Dangologo.png";
 
 const Settings = () => {
   return (
     <div>
-        <Navbar />
-        <h1>Settings</h1>
-    </div>
-  )
-}
+      <Navbar />
+      <div className="wrapper">
+        <section className="sidebar-container">
+          <div className="logo">
+            <img src={Dangologo} alt="DangoDot Logo" />
+            <h1>DangoDot</h1>
+          </div>
 
-export default Settings
+          <h3>Profile Settings</h3>
+          <div className="sidebar-list">
+            <a href="#" className="sidebar-item">
+              Profile Picture
+            </a>
+            <a href="#" className="sidebar-item">
+              Display Name
+            </a>
+          </div>
+
+          <h3>Account Settings</h3>
+          <div className="sidebar-list">
+            <a href="#" className="sidebar-item">
+              Username/Password
+            </a>
+            <a href="#" className="sidebar-item">
+              Email
+            </a>
+            <a href="#" className="sidebar-item">
+              Membership
+            </a>
+            <a href="#" className="sidebar-item">
+              Language
+            </a>
+            <a href="#" className="sidebar-item">
+              Theme
+            </a>
+          </div>
+        </section>
+
+        <section className="content-container">
+          <div className="content">
+            <h1>Settings</h1>
+            <p>Coming Soon!</p>
+          </div>
+        </section>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Settings;
