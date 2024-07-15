c2b55cd4a4539a2bcad7913c0234c61a200d6458
locationcyberia <brianna.corker@protonmail.com>
2024-07-15 01:27:22 -0400

    update content and styling

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,55 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Sidebar from "../components/Sidebar";
+import EditProfile from "../components/EditProfile";
+import UpdateAccount from "../components/UpdateAccount";
 import "../styles/settings.css";
 
-import Dangologo from "../assets/Dangologo.png";
-
 const Settings = () => {
+  const [content, setContent] = useState(
+    "Nothing here? Check out the sidebar!"
+  );
+
+  const renderContent = () => {
+    switch (content) {
+      case "EditProfile":
+        return <EditProfile />;
+      case "UpdateAccount":
+        return <UpdateAccount />;
+      case "Language":
+        return <div>Language content goes here.</div>;
+      case "Theme":
+        return <div>Theme content goes here.</div>;
+      default:
+        return <p>{content}</p>;
+    }
+  };
+
   return (
     <div>
       <Navbar />
       <div className="wrapper">
-        <section className="sidebar-container">
-          <div className="logo">
-            <img src={Dangologo} alt="DangoDot Logo" />
-            <h1>DangoDot</h1>
-          </div>
-
-          <h3>Profile Settings</h3>
-          <div className="sidebar-list">
-            <a href="#" className="sidebar-item">
-              Profile Picture
-            </a>
-            <a href="#" className="sidebar-item">
-              Display Name
-            </a>
-          </div>
-
-          <h3>Account Settings</h3>
-          <div className="sidebar-list">
-            <a href="#" className="sidebar-item">
-              Username/Password
-            </a>
-            <a href="#" className="sidebar-item">
-              Email
-            </a>
-            <a href="#" className="sidebar-item">
-              Membership
-            </a>
-            <a href="#" className="sidebar-item">
-              Language
-            </a>
-            <a href="#" className="sidebar-item">
-              Theme
-            </a>
-          </div>
-        </section>
-
+        <Sidebar onContentChange={setContent} />
         <section className="content-container">
           <div className="content">
             <h1>Settings</h1>
-            <p>Coming Soon!</p>
+            {renderContent()}
           </div>
         </section>
       </div>
