0d9d29d658d03093b07dc75acc1442550ea0b996
abopoku <aba.opoku@gmail.com>
2024-07-25 20:18:43 -0500

    set up database for login/signup via express application. Next need to continue to build database and make functional via axos or fetch

---
+++ express-application/schema/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    displayname: {
+        type: String,
+    }
+});
+
+const usermodel = mongoose.model('user', userSchema);
+module.exports = usermodel;
