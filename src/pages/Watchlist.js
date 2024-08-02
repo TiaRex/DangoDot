d1f7bc4162dac4a8d9e8be078f4c45f6174705ce
TiaRex <79944570+TiaRex@users.noreply.github.com>
2024-08-01 19:30:12 -0500

    Added Search Bar fetching data from API

--- src/pages/Watchlist.js
+++ src/pages/Watchlist.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar';
 import '../styles/Watchlist.css';
 import JujutsuKaisen from '../assets/Jujutsu-Kaisen.png';
 import DemonSlayer from '../assets/demonslayer.jpeg';
+import Footer from '../components/Footer';
 
 const Watchlist = () => {
+  const [search, setSearch] = useState('Naruto')
+  const [animeData, setAnimeData] = useState()
+
+  const getData = async() => {
+    const res=await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=1`)
+    const resData= await res.json();
+    setAnimeData(resData.data)
+    console.log(resData.data)
+  }
+  useEffect(()=>{
+    getData()
+  },[search])
+
   return (
     <body>
         <Navbar />
+
         <header>
             <div class="watchlist-container">
                 <h1 class="header-title">My Watchlists</h1>
+                <div className = "search-box">
+                  <input type = "search" placeholder = "Search Anime"
+                  onChange = {(e) => setSearch(e.target.value)}/>
+                </div>
             </div>
         </header>
 
@@ -38,6 +57,8 @@ const Watchlist = () => {
           </section>
           <br></br>
         </main>
+
+        <Footer />
     </body>
   )
 }
